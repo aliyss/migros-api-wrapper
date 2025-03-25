487ff45a33811509ca891935c1c80884a909436f
aliyss <silyas.bieri@lagomio.com>
2025-03-25 01:30:07 +0100

    tls modification reset

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -4,7 +4,7 @@ import { ICookies } from "../api/interfaces/cookies";
 
 import tls from "tls";
 import axios, { AxiosResponse } from "axios";
-tls.DEFAULT_MIN_VERSION = "TLSv1.3";
+import { exec } from "child_process";
 
 export async function getRequest(
   url: string,
@@ -31,10 +31,60 @@ export async function getRequestBypass(
   url = appendParametersToUrl(url, options);
   headers = addCookieToHeaders(headers, cookies);
 
-  return await axios(url, {
+  const previousMinVersion = tls.DEFAULT_MIN_VERSION;
+
+  tls.DEFAULT_MIN_VERSION = "TLSv1.3";
+
+  const response = await axios(url, {
     method: "GET",
     headers: headers,
   });
+
+  tls.DEFAULT_MIN_VERSION = previousMinVersion;
+
+  return response;
+}
+
+const execPromise = (cmd: string) => {
+  return new Promise(function (resolve, reject) {
+    exec(cmd, function (err: any, stdout: any) {
+      if (err) return reject(err);
+
+      resolve(stdout);
+    });
+  });
+};
+
+export async function getRequestBypassStrong(
+  url: string,
+  options: Record<string, string>,
+  headers: Record<string, string> = {},
+  cookies: ICookies = {},
+): Promise<{
+  headers: Record<string, string>;
+  data: string;
+}> {
+  url = appendParametersToUrl(url, options);
+  headers = addCookieToHeaders(headers, cookies);
+
+  const response = (await execPromise(
+    `curl -v --tlsv1.3 ${url} ${Object.keys(headers)
+      .map((key) => `-H "${key}: ${headers[key]}"`)
+      .join(" ")} -D -`,
+  )) as string;
+
+  const [headersString, data] = response.split("\r\n\r\n");
+  const headersArray = headersString.split("\r\n").slice(1);
+  const headersObject: Record<string, string> = {};
+  headersArray.forEach((header) => {
+    const [key, value] = header.split(": ");
+    headersObject[key] = value;
+  });
+
+  return {
+    headers: headersObject,
+    data: data,
+  };
 }
 
 export async function postRequest(
@@ -47,10 +97,9 @@ export async function postRequest(
   url = appendParametersToUrl(url, options);
   headers = addCookieToHeaders(headers, cookies);
 
-  const response = await fetch(url, {
+  return await fetch(url, {
     method: "POST",
     body: JSON.stringify(body) || undefined,
     headers: headers,
   });
-  return response;
 }
