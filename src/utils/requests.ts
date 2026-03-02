8a09ca87ee3d2fb5ca54c2be5d32fb6e0b7549c8
aliyss <silyas.bieri@lagomio.com>
2026-03-02 04:43:00 +0100

    remove curl

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -3,6 +3,13 @@ import { addCookieToHeaders } from "./addCookieToHeaders";
 import { ICookies } from "../api/interfaces/cookies";
 
 import axios, { AxiosResponse } from "axios";
+import https from "https";
+
+const axiosClient = axios.create({
+  httpsAgent: new https.Agent({
+    minVersion: "TLSv1.3",
+  }),
+});
 
 export async function getRequest(
   url: string,
@@ -29,74 +36,19 @@ export async function getRequestBypass(
   headers: AxiosResponse["headers"];
   data: AxiosResponse["data"];
 }> {
-  const tls = await import("tls");
   if (!headers["User-Agent"] && process.env.MIGROS_API_WRAPPER_USERAGENT) {
     headers["User-Agent"] = process.env.MIGROS_API_WRAPPER_USERAGENT;
   }
 
-  if (process.env.MIGROS_API_WRAPPER_USECURL) {
-    console.warn("Using curl to bypass cloudflare protection.");
-    return getRequestBypassStrong(url, options, headers, cookies);
-  }
-
   url = appendParametersToUrl(url, options);
   headers = addCookieToHeaders(headers, cookies);
 
-  const previousMinVersion = tls.DEFAULT_MIN_VERSION;
-
-  tls.DEFAULT_MIN_VERSION = "TLSv1.3";
-
-  const response = await axios(url, {
+  const response = await axiosClient(url, {
     method: "GET",
     headers: headers,
   });
 
-  tls.DEFAULT_MIN_VERSION = previousMinVersion;
-
   return response;
-}
-
-const execPromise = async (cmd: string) => {
-  const exec = await import("child_process").then((mod) => mod.exec);
-  return new Promise(function (resolve, reject) {
-    exec(cmd, function (err: any, stdout: any) {
-      if (err) return reject(err);
-
-      resolve(stdout);
-    });
-  });
-};
-
-export async function getRequestBypassStrong(
-  url: string,
-  options: Record<string, string>,
-  headers: Record<string, string> = {},
-  cookies: ICookies = {},
-): Promise<{
-  headers: AxiosResponse["headers"];
-  data: AxiosResponse["data"];
-}> {
-  url = appendParametersToUrl(url, options);
-  headers = addCookieToHeaders(headers, cookies);
-
-  const response = (await execPromise(
-    `curl -v --tlsv1.3 ${url} ${Object.keys(headers)
-      .map((key) => `-H "${key}: ${headers[key]}"`)
-      .join(" ")} -D -`,
-  )) as string;
-
-  const [headersString, data] = response.split("\r\n\r\n");
-  const headersArray = headersString.split("\r\n").slice(1);
-  const headersObject: Record<string, string> = {};
-  headersArray.forEach((header) => {
-    const [key, value] = header.split(": ");
-    headersObject[key] = value;
-  });
-
-  return {
-    headers: headersObject,
-    data: JSON.parse(data),
-  };
 }
 
 export async function postRequest(
@@ -126,63 +78,18 @@ export async function postRequestBypass(
   headers: AxiosResponse["headers"];
   data: AxiosResponse["data"];
 }> {
-  const tls = await import("tls");
   if (!headers["User-Agent"] && process.env.MIGROS_API_WRAPPER_USERAGENT) {
     headers["User-Agent"] = process.env.MIGROS_API_WRAPPER_USERAGENT;
-  }
-
-  if (process.env.MIGROS_API_WRAPPER_USECURL) {
-    console.warn("Using curl to bypass cloudflare protection.");
-    return postRequestBypassStrong(url, body, options, headers, cookies);
   }
 
   url = appendParametersToUrl(url, options);
   headers = addCookieToHeaders(headers, cookies);
 
-  const previousMinVersion = tls.DEFAULT_MIN_VERSION;
-
-  tls.DEFAULT_MIN_VERSION = "TLSv1.3";
-
-  const response = await axios(url, {
+  const response = await axiosClient(url, {
     method: "POST",
     data: body,
     headers: headers,
   });
 
-  tls.DEFAULT_MIN_VERSION = previousMinVersion;
-
   return response;
-}
-
-export async function postRequestBypassStrong(
-  url: string,
-  body: Record<string, any> | null,
-  options: Record<string, string>,
-  headers: Record<string, string> = {},
-  cookies: ICookies = {},
-): Promise<{
-  headers: AxiosResponse["headers"];
-  data: AxiosResponse["data"];
-}> {
-  url = appendParametersToUrl(url, options);
-  headers = addCookieToHeaders(headers, cookies);
-
-  const response = (await execPromise(
-    `curl -v --tlsv1.3 -X POST ${url} ${Object.keys(headers)
-      .map((key) => `-H "${key}: ${headers[key]}"`)
-      .join(" ")} --raw-data '${JSON.stringify(body)}' -D -`,
-  )) as string;
-
-  const [headersString, data] = response.split("\r\n\r\n");
-  const headersArray = headersString.split("\r\n").slice(1);
-  const headersObject: Record<string, string> = {};
-  headersArray.forEach((header) => {
-    const [key, value] = header.split(": ");
-    headersObject[key] = value;
-  });
-
-  return {
-    headers: headersObject,
-    data: JSON.parse(data),
-  };
 }
